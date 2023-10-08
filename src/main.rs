be31329039b4701d0de0c03baa265dfb07153026
Timur Sultanaev <str.write@gmail.com>
2023-10-08 20:43:44 +0200

    further logging configs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ extern crate tuirealm;
 
 use std::env;
 use std::fs::File;
+use std::sync::atomic::AtomicBool;
+use std::sync::mpsc::{self, Receiver, Sender};
+use std::sync::Mutex;
 
 use app::EditorId;
 use once_cell::sync::Lazy;
@@ -76,9 +79,14 @@ pub enum Id {
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
+static QUIT_CHANNEL: Lazy<Mutex<(Sender<String>, Receiver<String>)>> =
+    Lazy::new(|| Mutex::new(mpsc::channel()));
+
+static DEBUG_LOG: AtomicBool = AtomicBool::new(false);
+
 fn main() -> eyre::Result<()> {
     let args: Vec<_> = env::args().collect();
-    let mut debug_log = false;
+
     if args.len() > 1 {
         if args[1] == "--version" {
             println!("tisq v{}", VERSION);
@@ -86,32 +94,40 @@ fn main() -> eyre::Result<()> {
         }
 
         if args[1] == "--debug" {
-            debug_log = true;
+            DEBUG_LOG.store(true, std::sync::atomic::Ordering::Relaxed);
         }
     }
 
-    static DEBUG_FILE: Lazy<Option<File>> = Lazy::new(|| Some(File::create("debug.log").unwrap()));
-    static ERROR_FILE: Lazy<Option<File>> =
-        Lazy::new(|| Some(File::create("tisq-errors.log").unwrap()));
-    if debug_log {
-        tracing_subscriber::registry()
-            .with(
-                fmt::layer()
-                    .with_ansi(false)
-                    .with_writer(|| DEBUG_FILE.as_ref().unwrap()),
-            )
-            .with(EnvFilter::from_default_env().add_directive("tisq=debug".parse()?))
-            .init();
+    static LOG_FILE: Lazy<Option<File>> = Lazy::new(|| {
+        File::create(if DEBUG_LOG.load(std::sync::atomic::Ordering::Relaxed) {
+            "tisq-debug.log"
+        } else {
+            "tisq-errors.log"
+        })
+        .map_err(|_| {
+            QUIT_CHANNEL
+                .lock()
+                .unwrap()
+                .0
+                .send("Failed to create log file `tisq.log` - exited abnormally.".to_string())
+                .unwrap();
+        })
+        .ok()
+    });
+
+    let tracing_to_file = tracing_subscriber::registry().with(
+        fmt::layer()
+            .with_ansi(false)
+            .with_writer(|| LOG_FILE.as_ref().expect("log file not initialized!")),
+    );
+
+    if DEBUG_LOG.load(std::sync::atomic::Ordering::Relaxed) {
+        tracing_to_file.with(EnvFilter::from_default_env().add_directive("tisq=debug".parse()?))
     } else {
-        tracing_subscriber::registry()
-            .with(
-                fmt::layer()
-                    .with_ansi(false)
-                    .with_writer(|| ERROR_FILE.as_ref().unwrap()),
-            )
-            .with(EnvFilter::from_default_env())
-            .init();
+        // default level would be Error
+        tracing_to_file.with(EnvFilter::from_default_env())
     }
+    .init();
 
     // Setup model
     let mut model = Model::default();
@@ -119,6 +135,7 @@ fn main() -> eyre::Result<()> {
     // Enter alternate screen
     let _ = model.terminal.enter_alternate_screen();
     let _ = model.terminal.enable_raw_mode();
+    let mut quit_message = "".to_string();
     // Main loop
     // NOTE: loop until quit; quit is set in update if AppClose is received from counter
     while !model.quit {
@@ -146,15 +163,33 @@ fn main() -> eyre::Result<()> {
             }
             _ => {}
         }
-        // Redraw
-        if model.redraw {
-            model.view();
-            model.redraw = false;
+
+        let fatal_quit = QUIT_CHANNEL
+            .lock()
+            .map(|message| Some(message))
+            .unwrap_or(None)
+            .and_then(|it| it.1.try_iter().next());
+
+        if let Some(message) = fatal_quit {
+            // quitting with message
+            model.quit = true;
+            quit_message = message;
+        } else {
+            // Redraw
+            if model.redraw {
+                model.view();
+                model.redraw = false;
+            }
         }
     }
+
     // Terminate terminal
     let _ = model.terminal.leave_alternate_screen();
     let _ = model.terminal.disable_raw_mode();
     let _ = model.terminal.clear_screen();
+
+    if quit_message.len() > 0 {
+        println!("{}", quit_message);
+    }
     Ok(())
 }

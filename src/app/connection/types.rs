3b38c60edfde9e084e5643d9a6628b010b851b88
Timur Sultanaev <str.write@gmail.com>
2023-10-21 17:48:59 +0200

    debug log not supported type

--- src/app/connection/types.rs
+++ src/app/connection/types.rs
@@ -114,6 +114,7 @@ pub(super) trait GenericTypeWriter<'a, T, R, D>
 where
     R: Row<Database = D>,
     D: sqlx::Database<TypeInfo = T>,
+    T: Debug,
     bool: sqlx::Type<D> + sqlx::Decode<'a, D>,
     String: sqlx::Type<D> + sqlx::Decode<'a, D>,
     i8: sqlx::Type<D> + sqlx::Decode<'a, D>,
@@ -161,6 +162,7 @@ where
         if Self::write_via_debug::<()>(type_info, row, i, data) {
             return;
         }
+        tracing::debug!("Type not supported: {:?}", type_info);
         data.push("not supported".to_string());
 
         // TODO: Add support for other types

4b8f84e91348361052be9d1848b4233ca806a2f7
Timur Sultanaev <str.write@gmail.com>
2023-11-02 01:31:37 +0100

    use argh for CLI side

---
+++ src/tui.rs
@@ -0,0 +1,212 @@
+extern crate tuirealm;
+
+use std::env;
+use std::fs::File;
+use std::path::PathBuf;
+use std::sync::atomic::AtomicBool;
+use std::sync::mpsc::{self, Receiver, Sender};
+use std::sync::Mutex;
+
+use crate::{app, files};
+use app::{DbRequest, EditorId};
+use once_cell::sync::Lazy;
+use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{fmt, EnvFilter};
+use tuirealm::application::PollStrategy;
+
+use tuirealm::{AttrValue, Attribute, Update};
+
+use app::model::Model;
+use uuid::Uuid;
+
+use crate::config::TisqConfig;
+
+// Let's define the messages handled by our app. NOTE: it must derive `PartialEq`
+#[derive(Debug, PartialEq)]
+pub(crate) enum Msg {
+    AppClose,
+
+    ExecuteQuery(EditorId, String, i32),
+    // ReconnectAndExecuteQuery(EditorId, String),
+    ReconnectAndRepeat(DbRequest),
+    ChangeFocus(Id),
+
+    NavigateRight,
+    NavigateLeft,
+    NavigateUp,
+    NavigateDown,
+
+    PreviousEditor,
+    NextEditor,
+    MoveTabLeft(EditorId),
+    MoveTabRight(EditorId),
+    CloseTab(EditorId),
+
+    CycleNavigation,
+
+    StartAddingServer,
+    FocusPreviousInput,
+    FocusNextInput,
+    SubmitAddServerForm,
+
+    DeleteBrowsedNode(String),
+
+    OpenDatabase(Uuid, String, i32),
+    OpenSchema {
+        server_id: Uuid,
+        database: String,
+        schema: String,
+        retries: i32,
+    },
+    OpenConnection(Uuid),
+    LoadDatabases(Uuid),
+
+    OpenQueryEditor(Uuid, String),
+
+    ShowFetchedTable,
+    ShowErrorResult,
+
+    // ShowSnippets,
+    EditorTryExpand(EditorId, String),
+
+    None,
+}
+
+// Let's define the component ids for our application
+#[derive(Debug, Eq, PartialEq, Clone, Hash)]
+pub enum Id {
+    Clock,
+    DigitCounter,
+    LetterCounter,
+    Label,
+    EditorPanel,
+    Editor(EditorId),
+    Tree,
+    GlobalListener,
+
+    EditorTabs,
+    QueryResultTable,
+
+    AddServerForm,
+    ServerNameInput,
+    ConnectionUrlInput,
+    FormSubmitListener,
+
+    ExecuteErrorResult,
+}
+
+static QUIT_CHANNEL: Lazy<Mutex<(Sender<String>, Receiver<String>)>> =
+    Lazy::new(|| Mutex::new(mpsc::channel()));
+
+static DEBUG_LOG: AtomicBool = AtomicBool::new(false);
+
+pub(crate) fn run(debug_logs: bool) -> eyre::Result<()> {
+    if debug_logs {
+        DEBUG_LOG.store(true, std::sync::atomic::Ordering::Relaxed);
+    }
+
+    static FILES_ROOT: Lazy<eyre::Result<PathBuf>> = Lazy::new(|| files::open_tisq_root());
+
+    static LOG_FILE: Lazy<Option<File>> = Lazy::new(|| {
+        File::create(if DEBUG_LOG.load(std::sync::atomic::Ordering::Relaxed) {
+            FILES_ROOT.as_ref().unwrap().join("tisq-debug.log")
+        } else {
+            FILES_ROOT.as_ref().unwrap().join("tisq-errors.log")
+        })
+        .map_err(|_| {
+            QUIT_CHANNEL
+                .lock()
+                .unwrap()
+                .0
+                .send("Failed to create log file `tisq.log` - exited abnormally.".to_string())
+                .unwrap();
+        })
+        .ok()
+    });
+
+    let tracing_to_file = tracing_subscriber::registry().with(
+        fmt::layer()
+            .with_ansi(false)
+            .with_writer(|| LOG_FILE.as_ref().expect("log file not initialized!")),
+    );
+
+    if DEBUG_LOG.load(std::sync::atomic::Ordering::Relaxed) {
+        tracing_to_file.with(EnvFilter::from_default_env().add_directive("tisq=debug".parse()?))
+    } else {
+        // default level would be Error
+        tracing_to_file.with(EnvFilter::from_default_env())
+    }
+    .init();
+
+    let files_root = match FILES_ROOT.as_ref() {
+        Ok(root) => root,
+        Err(err) => return Err(eyre::eyre!("Failed to open tisq root directory: {}", err)),
+    };
+
+    let config = TisqConfig::read_or_create(files_root)?;
+
+    // Setup model
+    let mut model = Model::new(files_root, config);
+
+    // Enter alternate screen
+    let _ = model.terminal.enter_alternate_screen();
+    let _ = model.terminal.enable_raw_mode();
+    let mut quit_message = "".to_string();
+    // Main loop
+    // NOTE: loop until quit; quit is set in update if AppClose is received from counter
+    while !model.quit {
+        // Tick
+        match model.app.tick(PollStrategy::Once) {
+            Err(err) => {
+                assert!(model
+                    .app
+                    .attr(
+                        &Id::Label,
+                        Attribute::Text,
+                        AttrValue::String(format!("Application error: {}", err)),
+                    )
+                    .is_ok());
+            }
+            Ok(messages) if messages.len() > 0 => {
+                // NOTE: redraw if at least one msg has been processed
+                model.redraw = true;
+                for msg in messages.into_iter() {
+                    let mut msg = Some(msg);
+                    while msg.is_some() {
+                        msg = model.update(msg);
+                    }
+                }
+            }
+            _ => {}
+        }
+
+        let fatal_quit = QUIT_CHANNEL
+            .lock()
+            .map(|message| Some(message))
+            .unwrap_or(None)
+            .and_then(|it| it.1.try_iter().next());
+
+        if let Some(message) = fatal_quit {
+            // quitting with message
+            model.quit = true;
+            quit_message = message;
+        } else {
+            // Redraw
+            if model.redraw {
+                model.view();
+                model.redraw = false;
+            }
+        }
+    }
+
+    // Terminate terminal
+    let _ = model.terminal.leave_alternate_screen();
+    let _ = model.terminal.disable_raw_mode();
+    let _ = model.terminal.clear_screen();
+
+    if quit_message.len() > 0 {
+        println!("{}", quit_message);
+    }
+    Ok(())
+}
